61694be6eaf9b43f1309ea326c9cf6dbc402a926
Fajar <fajar@local>
2025-11-23 09:55:05 -0600

    Fix ABOV3 icon visibility: add theme-aware logo rendering

--- src/common/components/icons/vendors/ABOV3Icon.tsx
+++ src/common/components/icons/vendors/ABOV3Icon.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
 
-import { SvgIconProps } from '@mui/joy';
+import { SvgIconProps, useColorScheme } from '@mui/joy';
 
 export function ABOV3Icon(props: SvgIconProps) {
   const { fontSize, sx, ...otherProps } = props;
+  const { mode } = useColorScheme();
   const size = fontSize === 'xl' ? '32px' : '24px';
+
+  // Use black logo for light mode, white logo for dark mode
+  const logoSrc = mode === 'dark' ? '/images/abov3-logo-white.png' : '/images/abov3-logo-black.png';
 
   return (
     <img
-      src='/images/abov3-logo.png'
+      src={logoSrc}
       alt='ABOV3'
       style={{
         width: size,
         height: size,
         objectFit: 'contain',
-        // Remove black background using blend modes
-        mixBlendMode: 'screen',
-        opacity: 0.9,
       }}
       className={otherProps.className}
       id={otherProps.id}
